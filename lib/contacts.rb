cc1cc831b839279a5f0fba8af9e40993de71f28b
Harleigh Abel <harleighabel@gmail.com>
2018-02-09 12:04:14 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,27 +14,8 @@ require 'pry'
   #   }
   # }
   def remove_strawberry(
-  
-  def remove_strawberry(contacts_hash)
-    binding.pry
-    contacts_hash.each do |person, attribute|
-      attribute.each do |detail, data|
-        if data == "strawberry"
-          data.delete
-        end
-      end
-    end
-    contacts_hash
+    
   end  
   
-  
-=begin    
-contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-    if attribute == :favorite_ice_cream_flavors
-      data.delete_if {|ice_cream| ice_cream == "strawberry"}
-    end
-  end
-end
-=end
+
   
